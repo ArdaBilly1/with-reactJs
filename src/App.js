18d62edb096836c7e98ca5575785049e38215502
Ardabilli1 <billya749@gmail.com>
2020-08-30 23:01:10 +0700

    sign form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import './App.css';
+import signup from './pages/signup';
 import login from './pages/login';
+// import home from './pages/home';
 
 function App() {
   return (
@@ -9,7 +11,9 @@ function App() {
       <Router>
         <div>
           <Switch>
+            {/* <Route exact path="/home" component={home}/> */}
             <Route exact path="/login" component={login}/>
+            <Route exact path="/signup" component={signup}/>
           </Switch>
         </div>
       </Router>
